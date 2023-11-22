1b8db266c486a7481ac5c791a3f2ab1cf13bf0e7
Mehul Makwana <mehul.makwana@tatadigital.com>
2023-11-22 16:28:16 +0530

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Mehul Makwana on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
